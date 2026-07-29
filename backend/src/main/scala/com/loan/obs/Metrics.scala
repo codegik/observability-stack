370b6bd9d98dc3dc16de28b89c97caf048beb80c
Thiago Nascimento <dungeoncraw@gmail.com>
2026-07-29 18:40:46 -0300

    Fix metrics layer configuration

--- backend/src/main/scala/com/loan/obs/Metrics.scala
+++ backend/src/main/scala/com/loan/obs/Metrics.scala
@@ -1,16 +1,12 @@
 package com.loan.obs
 
 import zio.*
-import zio.metrics.connectors.MetricsConfig
-import zio.metrics.connectors.prometheus.{PrometheusPublisher, prometheusLayer}
+import zio.metrics.connectors.prometheus.{PrometheusPublisher, publisherLayer}
 import zio.metrics.jvm.DefaultJvmMetrics
 
 object Metrics:
-  private val metricsConfig: ULayer[MetricsConfig] =
-    ZLayer.succeed(MetricsConfig(5.seconds))
-
   val layer: ZLayer[Any, Nothing, PrometheusPublisher] =
-    metricsConfig >>> prometheusLayer
+    publisherLayer
 
   val jvmMetricsLayer: ZLayer[Any, Throwable, Unit] =
     Runtime.enableRuntimeMetrics >>> DefaultJvmMetrics.live.unit
