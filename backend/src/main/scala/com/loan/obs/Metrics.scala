d787e4c04f00fbaf20629ed85a6f1468f81f6c15
Thiago Nascimento <dungeoncraw@gmail.com>
2026-08-01 18:32:19 -0300

    Include the server metrics layer and grafana dashboard configuration

--- backend/src/main/scala/com/loan/obs/Metrics.scala
+++ backend/src/main/scala/com/loan/obs/Metrics.scala
@@ -1,12 +1,16 @@
 package com.loan.obs
 
 import zio.*
-import zio.metrics.connectors.prometheus.{PrometheusPublisher, publisherLayer}
+import zio.metrics.connectors.MetricsConfig
+import zio.metrics.connectors.prometheus.{PrometheusPublisher, prometheusLayer, publisherLayer}
 import zio.metrics.jvm.DefaultJvmMetrics
 
 object Metrics:
+  private val metricsConfig: ULayer[MetricsConfig] =
+    ZLayer.succeed(MetricsConfig(5.seconds))
+
   val layer: ZLayer[Any, Nothing, PrometheusPublisher] =
-    publisherLayer
+    (metricsConfig ++ publisherLayer) >+> prometheusLayer
 
   val jvmMetricsLayer: ZLayer[Any, Throwable, Unit] =
     Runtime.enableRuntimeMetrics >>> DefaultJvmMetrics.live.unit
